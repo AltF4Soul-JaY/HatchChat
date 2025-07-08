267c53c7744c5c2dba53f9a792c58562373aeeb4
Naman Singh <107694918+AltF4Soul-JaY@users.noreply.github.com>
2025-07-08 18:38:37 +0530

    Create centralized Firebase configuration

--- scripts/script.js
+++ scripts/script.js
@@ -17,19 +17,7 @@ import {
   goOffline,
   goOnline
 } from 'https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js';
-
-// ✅ Firebase Config
-console.log("env key:", import.meta.env.VITE_FIREBASE_API_KEY);
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-};
+import { firebaseConfig } from './firebase-config.js';
 
 // ✅ Initialize Firebase
 const app = initializeApp(firebaseConfig);
